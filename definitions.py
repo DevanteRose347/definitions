9008ab9377bd5d9843b0ba10a2a6c129a70e96de
Devante Rose <drose2021@yahoo.com>
2023-08-17 23:51:08 -0400

    first commit

---
+++ definitions.py
@@ -0,0 +1,39 @@
+# Question #1
+#### Difference between a Class and an Object
+
+
+# ANSWER #1
+# A class is a layout for an object.
+# An object is a replica or prototype of a class.
+
+# Question #2
+
+#### Encapsulation
+
+# Answer #2
+
+# storing methods or variable names in a 
+# class that has access to that information.
+
+
+# Question #3
+
+#### Inheritance
+
+
+# Answer #3
+# A son or daughter recieving traits from a parent. 
+# They are similar in a way.
+# The child has access to the parent.
+
+# Question #4
+
+#### Polymorphism
+
+#Answer #5
+#Bieng able to be used by different classes or objects connected to it.
+
+# Question #6
+
+#### Abstraction
+#Hidden data
